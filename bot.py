1e9f3416799b043c63dd796d5c67e2d70c06db97
Marco Rougeth <marco@rougeth.com>
2019-10-20 16:54:38 +0100

    Add command that returns Python Brasil address

--- bot.py
+++ bot.py
@@ -12,4 +12,14 @@ def hello_world(message):
     bot.reply_to(message, f"Hello, @{message.from_user.username}!")
 
 
+@bot.message_handler(commands=["local", "endereço", "endereco"])
+def address(message):
+    bot.send_message(message.chat.id,(
+        "As palestras da *Python Brasil 2019* acontecerão no endereço abaixo:\n"
+        "*Centro de Convenções Ribeirão Preto*\n"
+        "R. Bernardino de Campos, 999 - Centro, Ribeirão Preto - SP"
+    ), parse_mode="Markdown")
+    bot.send_location(message.chat.id, -21.1748969, -47.8098745)
+
+
 bot.polling()
