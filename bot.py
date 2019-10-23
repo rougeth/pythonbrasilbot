b922e03901b53a9c6ddc49bbde45c70f7f994e58
Marco Rougeth <marco@rougeth.com>
2019-10-23 12:52:40 -0300

    Add tutorials list

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telebot import TeleBot, types
 from decouple import config
 
-from utils import inline_keyboard
+from utils import inline_keyboard, get_tutorial_events
 
 TELEGRAM_TOKEN = config("TELEGRAM_TOKEN")
 
@@ -88,8 +88,9 @@ def select_activity_date(callback):
     elif callback.data == "grade_palestras":
         message = "Você deseja ver a programação das *palestras* de qual dia?"
         keyboard = inline_keyboard([
-            [("25 de Outubro", "grade_tutoriais_25")],
-            [("26 de Outubro", "grade_tutoriais_26")],
+            [("25 de Outubro", "grade_palestras_25")],
+            [("26 de Outubro", "grade_palestras_26")],
+            [("27 de Outubro", "grade_palestras_27")],
         ])
 
     bot.edit_message_text(
@@ -98,6 +99,47 @@ def select_activity_date(callback):
         callback.message.message_id,
         parse_mode="Markdown",
         reply_markup=keyboard,
+    )
+
+
+@bot.callback_query_handler(func=lambda callback: callback.data in [
+    "grade_tutoriais_23",
+    "grade_tutoriais_24",
+])
+def tutoriais(callback):
+    day = 23 if callback.data == "grade_tutoriais_23" else 24
+
+    bot.edit_message_text(
+        f"Programação para os *tutoriais* no dia {day}",
+        callback.message.chat.id,
+        callback.message.message_id,
+        parse_mode="Markdown",
+        reply_markup=None,
+    )
+
+    events = get_tutorial_events()
+    event_message_template = (
+        "*{title}*\n"
+        "- {author}\n"
+        "- {time}\n\n"
+    )
+
+    message = ""
+    for event in events:
+        if event["start"]["dateTime"].day != day:
+            continue
+
+        message += event_message_template.format(
+            title=event["summary"],
+            author=event["extendedProperties"]["private"]["author"],
+            time=event["start"]["dateTime"],
+        )
+
+    bot.send_message(
+        callback.message.chat.id,
+        message,
+        parse_mode="Markdown",
+        reply_markup=None,
     )
 
 
--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+
+import requests
+from decouple import config
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
@@ -14,3 +18,21 @@ def inline_keyboard(menu):
         keyboard.row(*buttons)
 
     return keyboard
+
+
+def get_calendar_events():
+    content = requests.get(config("CALENDAR_URL"))
+    return content.json()["items"]
+
+
+def get_tutorial_events():
+    events = get_calendar_events()
+    tutorials = []
+    for event in events:
+        start = event["start"]["dateTime"]
+        start = datetime.fromisoformat(start)
+        if start.day in [23, 24]:
+            event["start"]["dateTime"] = start
+            tutorials.append(event)
+
+    return tutorials
