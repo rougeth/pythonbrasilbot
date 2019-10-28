63a65f62abba1d01badaaf930b028c70f86a3f33
John Carneiro <john@sizy.com.br>
2019-10-28 16:16:39 -0300

    Adiciona funções para grade

--- pythonbrasilbot/database.py
+++ pythonbrasilbot/database.py
@@ -1,5 +1,6 @@
 import os
 import json
+from datetime import datetime
 
 
 def get_content(year):
@@ -9,6 +10,30 @@ def get_content(year):
         return json.load(json_file)
 
 
-def get_grade_opcoes(year):
-    content = get_content(year)
-    return content['grade'].keys()
+def grade_chave(chave):
+    return f"grade_{chave}"
+
+
+def grade_chaves(content):
+    chaves = content['grade'].keys()
+    return [grade_chave(i) for i in chaves]
+
+
+def get_grade_opcoes(content):
+    try:
+        chaves = content['grade'].keys()
+
+        opcoes = []
+        for chave in chaves:
+            label = content['grade'][chave]['label']
+            chave = grade_chave(chave)
+            opcoes.append([(label, chave)])
+
+        return opcoes
+    except FileNotFoundError:
+        return []
+
+
+now = datetime.now()
+current_year = now.year
+content = get_content(current_year)
